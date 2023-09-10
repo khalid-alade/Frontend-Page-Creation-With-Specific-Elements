5b294f25b8a4535a66d14a5d7cb9b2f5f5fcad1d
Yusuf Khalid <khalidalade2004@gmail.com>
2023-09-10 15:59:11 +0100

    Fourth commit

--- t1.js
+++ t1.js
@@ -6,11 +6,15 @@ var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'S
 const date = new Date()
 let day = date.getDay()
 dayNum = Number(day)
+
+var year = date.getUTCFullYear()
+var month = date.getUTCMonth() + 1
+var utcDay = date.getUTCDate()
 utcHour = date.getUTCHours()
 utcMin = date.getUTCMinutes()
 utcSec = date.getUTCSeconds()
 utcMilli = date.getUTCMilliseconds()
-utcTime = `${utcHour}:${utcMin}:${utcSec}:${utcMilli}`
+utcTime = `${year}-${month}-${utcDay} ${utcHour}:${utcMin}:${utcSec}.${utcMilli}`
 
 weekDay.innerHTML = days[dayNum]
 time.innerHTML = utcTime
