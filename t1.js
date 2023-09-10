24d195ef839b4c310c83405b9c58f2a5f19c5482
Yusuf Khalid <khalidalade2004@gmail.com>
2023-09-10 16:43:16 +0100

    Fifth commit

--- t1.js
+++ t1.js
@@ -3,20 +3,23 @@ var weekDay = ps[0]
 var time = ps[1]
 var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
 
-const date = new Date()
-let day = date.getDay()
-dayNum = Number(day)
+function updateTime() {
+    const date = new Date()
+    let day = date.getDay()
+    dayNum = Number(day)
 
-var year = date.getUTCFullYear()
-var month = date.getUTCMonth() + 1
-var utcDay = date.getUTCDate()
-utcHour = date.getUTCHours()
-utcMin = date.getUTCMinutes()
-utcSec = date.getUTCSeconds()
-utcMilli = date.getUTCMilliseconds()
-utcTime = `${year}-${month}-${utcDay} ${utcHour}:${utcMin}:${utcSec}.${utcMilli}`
+    var year = date.getUTCFullYear()
+    var month = date.getUTCMonth() + 1
+    var utcDay = date.getUTCDate()
+    var utcHour = date.getUTCHours()
+    var utcMin = date.getUTCMinutes()
+    var utcSec = date.getUTCSeconds()
+    var utcMilli = date.getUTCMilliseconds()
+    var utcTime = `${year}-${month}-${utcDay} ${utcHour}:${utcMin}:${utcSec}.${utcMilli}`
 
-weekDay.innerHTML = days[dayNum]
-time.innerHTML = utcTime
+    weekDay.innerHTML = days[dayNum]
+    time.innerHTML = utcTime
 
-console.log(utcTime)
+}
+updateTime()
+setInterval(updateTime, 1000)
